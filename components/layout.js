a8ba5725128988a6882535b35e1a6179151b00d8
kidsdream <docomo905dream@yahoo.co.jp>
2021-08-12 23:58:01 +0900

    fix background use image module

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
 import styles from '/styles/layout.module.scss'
 import MediaQuery from "react-responsive";
+import Image from 'next/image'
 
 function Layout({ children }) {
   let d = new Date();
@@ -7,13 +8,14 @@ function Layout({ children }) {
   return (
     <>
       <div className={styles.body}>
+        <Image src="/images/background.jpg" layout="fill" objectFit="cover" alt="背景画像"/>
         <MediaQuery query="(min-width: 768px)">
           <header>
             <h1>DREMATER</h1>
           </header>
           {children}
           <footer>
-            <small>&copy; {year} DREMATER ver0.2.2</small>
+            <small>&copy; {year} DREMATER ver0.2.3</small>
           </footer>
         </MediaQuery>
         <MediaQuery query="(max-width: 767px)">
