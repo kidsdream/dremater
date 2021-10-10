6436bfb99d05cb999ee64261779fb8f918894e8f
kidsdream <docomo905dream@yahoo.co.jp>
2021-10-10 19:45:49 +0900

    XDの現状ファイルへ更新

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,26 +1,26 @@
 import styles from '/styles/layout.module.scss'
-import MediaQuery from "react-responsive";
 import Image from 'next/image'
+import Head from 'next/head'
 
 function Layout({ children }) {
   let d = new Date();
   let year = d.getFullYear();
   return (
     <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&display=swap" rel="stylesheet" />
+      </Head>
       <div className={styles.body}>
-        <Image src="/images/background.jpg" layout="fill" objectFit="cover" priority={true} alt="背景画像"/>
-        <MediaQuery query="(min-width: 768px)">
-          <header>
-            <h1>DREMATER</h1>
-          </header>
+        <Image src="/images/background.jpg" layout="fill" objectFit="cover" alt="背景画像" className={styles.background}/>
+        <header>
+          <h1>DREMATER</h1>
+        </header>
+        <main>
           {children}
-          <footer>
-            <small>&copy; {year} DREMATER ver0.2.3</small>
-          </footer>
-        </MediaQuery>
-        <MediaQuery query="(max-width: 767px)">
-          {children}
-        </MediaQuery>
+        </main>
+        <footer>
+          <small>&copy; {year} DREMATER</small>
+        </footer>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,55 @@
 import { client } from "../libs/client";
-import styles from '../styles/blog_list.module.scss';
+import styles from '../styles/index.module.scss';
 import Layout from '../components/layout'
 import Profile from '../components/profile'
+import NewsArea from '../components/newsArea'
 import BlogArea from '../components/blogArea'
 import Head from 'next/head'
-import MediaQuery from "react-responsive";
 import React from 'react';
+import Link from "next/link";
 
-export default function Home({ blogs }) {
+export default function Home({ newses, blogs }) {
   return (
     <>
-      <MediaQuery query="(min-width: 768px)">
-        <Layout>
-          <Head>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <title>DREMATER</title>
-          </Head>
-          <Profile></Profile>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>DREMATER</title>
+      </Head>
+      <Layout>
+        <Profile></Profile>
+        <div className={styles.mainContentsArea}>
+          <div className={styles.contentHeader}>
+            <div className={styles.titleArea}>
+              <h2 className={styles.mainTitle}>NEWS</h2>
+              <p className={styles.subTitle}>お知らせ</p>
+            </div>
+            <Link href="#">
+              <a className={styles.listButton}>お知らせ一覧</a>
+            </Link>
+          </div>
+          <div className={styles.newsList}>
+            {newses.map((news, index) => (
+              <NewsArea key={index} value={news} >
+              </NewsArea>
+            ))}
+          </div>
+          <div className={`${styles.contentHeader} ${styles.margintop40}`}>
+            <div className={styles.titleArea}>
+              <h2 className={styles.mainTitle}>BLOG</h2>
+              <p className={styles.subTitle}>ブログ</p>
+            </div>
+            <Link href="#">
+              <a className={styles.listButton}>ブログ一覧</a>
+            </Link>
+          </div>
           <div className={styles.blogList}>
             {blogs.map((blog, index) => (
               <BlogArea key={index} value={blog} >
               </BlogArea>
             ))}
           </div>
-        </Layout>
-      </MediaQuery>
-      <MediaQuery query="(max-width: 767px)">
-        <Layout>
-          <Head>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <title>DREMATER</title>
-          </Head>
-          <div className={styles.blogList}>
-            {blogs.map((blog, index) => (
-              <BlogArea key={index} value={blog} >
-              </BlogArea>
-            ))}
-          </div>
-          <Profile></Profile>
-        </Layout>
-      </MediaQuery>
+        </div>
+      </Layout>
     </>
   );
 
@@ -47,10 +57,18 @@ export default function Home({ blogs }) {
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
-  const data: any = await client.get({ endpoint: "blog" });
+  const blogData: any = await client.get({
+    endpoint: "blog",
+    queries: { limit: 10 }
+  });
+  const newsData: any = await client.get({
+    endpoint: "news",
+    queries: { limit: 3 }
+  });
   return {
     props: {
-      blogs: data.contents,
+      blogs: blogData.contents,
+      newses: newsData.contents,
     },
   };
 };
