67faad971f17828ad028abc94cb495d730f3659a
kidsdream <docomo905dream@yahoo.co.jp>
2021-10-12 17:24:54 +0900

    ハンバーガーメニューを追加

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import styles from '/styles/layout.module.scss'
 import Image from 'next/image'
 import Head from 'next/head'
+import Menu from './menu'
 
 function Layout({ children }) {
   let d = new Date();
@@ -10,12 +11,13 @@ function Layout({ children }) {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&display=swap" rel="stylesheet" />
       </Head>
-      <div className={styles.body}>
-        <Image src="/images/background.jpg" layout="fill" objectFit="cover" alt="背景画像" className={styles.background}/>
+      <div className={styles.body} id="outer-container">
+        <Image src="/images/background.jpg" layout="fill" objectFit="cover" alt="背景画像" className={styles.background} />
+        <Menu width={ 260 } pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } />
         <header>
           <h1>DREMATER</h1>
         </header>
-        <main>
+        <main id="page-wrap">
           {children}
         </main>
         <footer>
